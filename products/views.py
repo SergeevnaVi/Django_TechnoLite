ebed1f0e553044def2e753f64157374f77407368
Violetta <borshovavioletta2006@gmail.com>
2024-12-02 00:03:56 +0300

    # Simple search create

--- products/views.py
+++ products/views.py
@@ -1,27 +1,28 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Products
+from .utils import q_search
 
-def catalog(request, category_slug):
-
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'vse-tovary':
         products = Products.objects.all()
+    elif query:
+        products = q_search(query)
     else:
         products = Products.objects.filter(category__slug=category_slug)
 
     if on_sale:
         products = products.filter(discount__gt=0)
-
     if order_by and order_by != 'default':
         products = products.order_by(order_by)
 
     paginator = Paginator(products, 3)
     current_page = paginator.page(int(page))
-
     context = {
         'title': 'TechnoLite - Главная',
         'products': current_page,
@@ -31,9 +32,7 @@ def catalog(request, category_slug):
 
 
 def product(request, product_slug):
-
     product = Products.objects.get(slug=product_slug)
-
     context = {
         'product': product
     }
