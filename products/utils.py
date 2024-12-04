40455a63b0c2c6460f0c69e4f22b777b9a675b0c
Violetta <borshovavioletta2006@gmail.com>
2024-12-04 18:46:59 +0300

    # Full text search

--- products/utils.py
+++ products/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector
 
 from products.models import Products
 
@@ -7,13 +8,16 @@ from products.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    # Поисковая строка по названию и описанию товара
+    return Products.objects.annotate(search=SearchVector("name", "description"),).filter(search=query)
 
-    keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
-
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
