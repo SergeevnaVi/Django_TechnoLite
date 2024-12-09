37c1b360b1f14dccfa2914b2a8bc3d0eacb05f3b
Violetta <borshovavioletta2006@gmail.com>
2024-12-09 15:06:21 +0300

    # The edit validate_phone_number

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.template.loader import render_to_string
 from products.models import Products
 from carts.models import Cart
@@ -57,9 +57,13 @@ def cart_change(request):
     cart.save()
     updated_quantity = cart.quantity
 
-    cart = get_user_carts(request)
+    user_cart = get_user_carts(request)
+    context = {"carts": user_cart}
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["order"] = True
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': cart}, request=request)
+        'carts/includes/included_cart.html', context, request=request)
 
     response_data = {
         'message': 'Количество изменено',
@@ -77,8 +81,12 @@ def cart_remove(request):
     cart.delete()
 
     user_cart = get_user_carts(request)
+    context = {"carts": user_cart}
+    referer = request.META.get('HTTP_REFERER')
+    if reverse('orders:create_order') in referer:
+        context["order"] = True
     cart_items_html = render_to_string(
-        'carts/includes/included_cart.html', {'carts': user_cart}, request=request)
+        'carts/includes/included_cart.html', context, request=request)
 
     response_data = {
         'message': 'Товар удален',
